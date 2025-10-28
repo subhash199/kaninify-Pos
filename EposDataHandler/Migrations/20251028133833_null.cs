4d4f48ab3982e637d36b5b808ad7af2bff5e8736
Your Name <you@example.com>
2025-10-28 13:40:10 +0000

    migration commit

---
+++ EposDataHandler/Migrations/20251028133833_null.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataHandlerLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class @null : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Printer_Port_Number",
+                table: "ReceiptPrinters",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Printer_Port_Number",
+                table: "ReceiptPrinters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
