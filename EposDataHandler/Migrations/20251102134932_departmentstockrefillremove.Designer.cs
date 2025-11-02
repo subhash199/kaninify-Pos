fd0aeec25fa16ac89eab1ab7f805da4dfba0761d
Your Name <you@example.com>
2025-11-02 13:50:16 +0000

    department migration

--- EposDataHandler/Migrations/20251102134932_departmentstockrefillremove.Designer.cs
+++ EposDataHandler/Migrations/20251102134932_departmentstockrefillremove.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EntityFrameworkDatabaseLibrary.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DataHandlerLibrary.Migrations
 {
     [DbContext(typeof(DatabaseInitialization))]
-    partial class DatabaseInitializationModelSnapshot : ModelSnapshot
+    [Migration("20251102134932_departmentstockrefillremove")]
+    partial class departmentstockrefillremove
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EposDataHandler/Migrations/20251102134932_departmentstockrefillremove.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataHandlerLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class departmentstockrefillremove : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stock_Refill_Print",
+                table: "Departments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Stock_Refill_Print",
+                table: "Departments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
