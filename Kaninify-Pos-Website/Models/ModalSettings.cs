f7221a7ecace20587553cc215371cbb454a47ee0
Your Name <you@example.com>
2026-02-12 18:17:16 +0000

    enquiry for mobile website and login merger conflict

---
+++ Kaninify-Pos-Website/Models/ModalSettings.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaninify_Pos_Website.Models
+{
+    public class ModalSettings
+    {
+        public bool ShowModal { get; set; } = false;
+        public string ModalTitle { get; set; } = "Notice";
+        public string ModalMessage { get; set; } = "This is a message.";
+        public ModalEnum ModalEnum { get; set; } = ModalEnum.Default;
+        public string SecondaryIconLink { get; set; } = string.Empty;
+    }
+    public enum ModalEnum
+    {
+        ProductActivate,
+        ProductNotFound,
+        GenericProduct,
+        Error,
+        Default,
+        Confirmation,
+        Refund,
+        Transaction,
+        ChangeDue
+    }
+
+}
